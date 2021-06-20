7820d3756cb55dd4dbe157a135aeacf58bc6e2fe
YingShuan Lin <ylin70@montgomerycollege.edu>
2021-06-20 00:23:28 -0400

    CMSC_203 Lab 1 task 1

---
+++ Eclipse_WS/CMSC203_Lab1/src/MovieDriver.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class MovieDriver {
+
+	public static void main(String[] args) {
+		Movie movie = new Movie();
+		Scanner scanner = new Scanner(System.in);
+
+		// task 1 
+		System.out.println("Enter the name of movie");
+	    movie.setTitle(scanner.nextLine());
+		
+		System.out.println("Enter the rating of the movie");
+		movie.setRating(scanner.nextLine());
+		
+		System.out.println("Enter the number of tickets sold for this movie");
+		movie.setSoldTickets(scanner.nextInt());
+		scanner.nextLine();
+		
+		String tooutstr = movie.toString();
+		System.out.println(tooutstr);
+		
+		System.out.println("Goodbye"); 
+		
+		// task 2		
+		/*
+		while (true)
+		{		
+		    System.out.println("Enter the name of movie");
+		    movie.setTitle(scanner.nextLine());
+    		
+    		System.out.println("Enter the rating of the movie");
+    		movie.setRating(scanner.nextLine());
+    		
+    		System.out.println("Enter the number of tickets sold for this movie");
+    		movie.setSoldTickets(scanner.nextInt());
+    		scanner.nextLine();
+    		
+    		String tooutstr = movie.toString();
+    		System.out.println(tooutstr);
+    		
+    		System.out.println("Do you want to enter another? (y or n)");
+    		
+		    if(scanner.nextLine().equals("n")){
+	            System.out.println("Goodbye");
+	            break;
+		    }
+		}    */
+		scanner.close();
+	} 
+
+}
